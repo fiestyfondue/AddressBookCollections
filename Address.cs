67d83e809826fd66d26129571fb797e53b86d362
Harsh <harsh.21483@gmail.com>
2021-06-27 09:50:03 -0700

    [Harsh]Add:-New class of Address and AddressBook is added

---
+++ Address.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionAddressBook
+{
+    class Address
+    {
+        public string FirstName;
+        public string LastName;
+        public string address;
+        public string City;
+        public string State;
+        public int Zip;
+        public int PhoneNo;
+        public string Email;
+
+        public Address(string firstName,string lastName, string address,string city,String state, int zip, int phoneNo,string email)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.address = address;
+            this.City = city;
+            this.State = state;
+            this.Zip = zip;
+            this.PhoneNo = phoneNo;
+            this.Email = email;
+        }
+    }
+}
